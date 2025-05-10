fbc0b34af627ea06e8c510850322d6dce55feae3
akumar <akumar@viveconcierge.net>
2025-05-10 13:49:42 +0530

    Login implementation, callbackUrl

--- db/seed.ts
+++ db/seed.ts
@@ -3,19 +3,31 @@ import sampleData from "./sample-data";
 
 async function main() {
     await prisma.product.deleteMany();
+    await prisma.user.deleteMany();
+    await prisma.account.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.verificationToken.deleteMany();
+
     await prisma.product.createMany({
         data: sampleData.products,
     });
+
     console.log("Products seeded successfully");
+
+    await prisma.user.createMany({
+        data: sampleData.users,
+    });
+
+    console.log("Users seeded successfully");
 }
 
 
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-    });
+    // .then(async () => {
+    //     await prisma.$disconnect();
+    // })
+    // .catch(async (e) => {
+    //     console.error(e);
+    //     await prisma.$disconnect();
+    // });
 
